2baaeb84273733440018ac6764bdaf14e6cc6d4e
Douglas Parker <me@douglas-parker.com>
2021-09-11 16:26:45 -0700

    add: initial commit

---
+++ changelog-parser.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+
+version = sys.argv[1]
+changelog = sys.argv[2]
+
+changelog_file = open(changelog, "r")
+changelog = changelog_file.read()
+changelog_file.close()
+
+version_string = f"## {version}"
+next_version_string = "## "
+
+changelog_start = changelog.rfind(version_string)
+
+changelog_end = changelog.find(next_version_string, changelog_start + len(version_string))
+
+while not changelog[changelog_end + len(next_version_string)].isnumeric():
+    changelog_end = changelog.find(next_version_string, changelog_end + len(next_version_string))
+
+print(changelog[changelog_start:changelog_end])
